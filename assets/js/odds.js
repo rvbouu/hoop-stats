ee30986846ba689b7e278a1231003b11abc2e8da
Vanessa Bou <rvbouu@gmail.com>
2024-04-11 13:27:12 -0500

    made JS for odds and comments

---
+++ assets/js/odds.js
@@ -0,0 +1,7 @@
+// JS file for what are the odds page
+// taking two teams and compares overall win percent
+// highlights winner in green
+// shows both teams overall stats on sides
+// at top: images for all teams that are drag/droppable
+// reset button to clear out comparison square
+// requestURL for individual teams :: http://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/{team abbr}
