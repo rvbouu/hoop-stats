2ea28979922363a74e0db51de23b7bd61d5452ad
Vanessa Bou <rvbouu@gmail.com>
2024-04-15 11:05:12 -0500

    edits made to some docs

--- assets/js/all-teams.js
+++ assets/js/all-teams.js
@@ -10,3 +10,12 @@ function saveNBAToStorage(nba) {
   let savedNBA = JSON.stringify(nba);
   localStorage.setItem('NBA', savedNBA);
 };
+
+// function populate team cards (logos)
+// make logos clickable
+// click takes you to NBA page
+
+// div to hold everything
+// append an a tag to div; add attr href with link
+// append img tag to a tag; image has logo href
+// append p tag to div; team names
