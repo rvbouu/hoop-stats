28dee7115aed67dcf4c6839de1460952dd8db008
JoelhansenMN <joelhansen22@gmail.com>
2024-04-15 12:39:26 -0500

    js work on all-teams page

--- assets/js/all-teams.js
+++ assets/js/all-teams.js
@@ -10,3 +10,22 @@ function saveNBAToStorage(nba) {
   let savedNBA = JSON.stringify(nba);
   localStorage.setItem('NBA', savedNBA);
 };
+
+function allTeams() {
+  const allTeams = $('<main>');
+  allTeams.addClass('col text-center').attr('id', 'allTeams');
+  const div = $('<div>');
+  div.addClass('fw-bold text-center').attr('id', 'team').appendTo(allTeams);
+  const input = $('<input>');
+  input.addClass('form-check-input').attr('id', 'check').appendTo(div);
+  const logo = $('<label>');
+  logo.addClass('form-check-label').text().attr('href', '').appendTo(div);
+  const img = $('<img>');
+  img.attr('src',team.logo).attr('alt', `${teams.team.logo} logo`).addClass('logos');
+  teamCard.append(img, div);
+
+
+  return allTeams;
+}
+
+allTeams();
