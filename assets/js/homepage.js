bd30551ac2848bad46fbd027142a5f14c1d12722
kyle butcher <kylembutcher97@gmail.com>
2024-04-16 12:52:09 -0500

    function to display favorite teams popping up when user login completes

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -30,7 +30,7 @@ function saveTeamsToStorage(teamArray) {
   let savedTeams = JSON.stringify(teamArray);
   localStorage.setItem('teamArray', savedTeams);
 }
-
+// gets upcoming games from the api saved in local storage
 function scheduleAPI() {
   const requestURL = 'https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard';
 
@@ -45,7 +45,7 @@ function scheduleAPI() {
       let events = data.events
       console.log(events);
 
-      
+
       for (let i = 0; i < events.length; i++) {
         let event = {
           date: events[i].status.type.detail,
@@ -53,11 +53,11 @@ function scheduleAPI() {
           description: events[i].status.type.description,
           clock: events[i].status.clock,
           period: events[i].status.period,
-    } 
-    schedule.push(event);
-  }
+        }
+        schedule.push(event);
+      }
       console.log(schedule);
-      for (let i = 0; i <schedule.length; i++) {
+      for (let i = 0; i < schedule.length; i++) {
         let sidebar = $(".sidebar");
         let div = $('<div>');
         div.appendTo(sidebar);
@@ -73,9 +73,60 @@ function scheduleAPI() {
         description.text(`${schedule[i].description}`).appendTo(div);
         // div.appendTo(sidebar);
       }
-    
-  })
-
+    })
 }
+// this function will be used to populate the users favorite teams
+let favTeams = [];
+function populateFavTeams() {
+  // let user = ['teams']
 
+// this function is getting the faocirte teams from the users selected teams in the info saved from the local storage.
+    let userTeamsLs = JSON.parse(localStorage.getItem('userTeams'));
+    let nbaTeams = JSON.parse(localStorage.getItem('NBA Teams'));
+    let userTeams = userTeamsLs[0];
+
+    for (let i = 0; i < userTeams.length; i++) {
+      let team = userTeams[i];
+      for (let i = 0; i < nbaTeams.length; i++) {
+        if (team == nbaTeams[i].team.displayName) {
+          let teamInfo = {
+            name: nbaTeams[i].team.displayName,
+            logo: nbaTeams[i].team.logos[0].href,
+            web: nbaTeams[i].team.links[0].href,
+            roster: nbaTeams[i].team.links[1].href,
+            shcedule: nbaTeams[i].team.links[3].href
+          }
+          favTeams.push(teamInfo);
+        }
+      }
+    }
+    console.log(favTeams);
+  }
+
+  
+  function createFavTeams(){
+    const teamDisplay = $('.row-content');
+    for(let i = 0; i < favTeams.length; i++){
+      let div = $('<div>');
+      let web = $('<a>');
+      web.attr('href', `${favTeams[i].web}`).appendTo(div);
+      let img = $('<img>');
+      img.attr('src', `${favTeams[i].logo}`).attr('alt', `${favTeams[i].name} logo`).appendTo(web);
+      let h4 = $('<h4>');
+      h4.text(`${favTeams[i].name}`).appendTo(div);
+      let roster = $('<a>');
+      roster.attr('href', `${favTeams[i].roster}`).text('Roster').appendTo(div);
+      let schedule = $('<a>');
+      schedule.attr('href', `${favTeams[i].schedule}`).text('Schedule').appendTo(div);
+      div.appendTo(teamDisplay);
+    }
+
+  }
+populateFavTeams();
+createFavTeams();
 scheduleAPI();
+  
+
+
+
+
