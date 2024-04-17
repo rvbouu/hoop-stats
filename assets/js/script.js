5d8cbd033b0cd8b4fba79dcc87807d5688f149ba
Vanessa Bou <rvbouu@gmail.com>
2024-04-17 09:47:04 -0500

    so many little things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,9 @@
-// gets NBA data from localStorage
-function readNBAFromStorage() {
-  let stringData = localStorage.getItem('NBA');
-  let nba = JSON.parse(stringData) || [];
-  return nba;
-}
-
-// saves NBA data to localStorage
-function saveNBAToStorage(nba) {
-  let savedNBA = JSON.stringify(nba);
-  localStorage.setItem('NBA', savedNBA);
-}
 // gets NBA data from api and saves it to localStorage
 function getApi() {
   // URL where we're requesting data from - ESPN NBA
   const requestURL = `https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams`;
+  console.log(requestURL);
+
   // console.log(requestURL);
   // fetch request gets a list of all teams in the NBA
   fetch(requestURL)
@@ -21,22 +11,38 @@ function getApi() {
       // converts a json string into a js object/array
       return response.json();
     })
+
     // saving converted data to localStorage 
     .then(data => {
+      console.log(data);
       // console.log(data);
-      localStorage.setItem('NBA Teams', JSON.stringify(data));
+      localStorage.setItem('NBA', JSON.stringify(data))
     })
+
     // catches error if occurs and alerts user that something when wrong
     .catch(function (error) {
       console.log(error);
       alert('An error has occured.');
     });
+
 }
-// calling getApi function
 getApi();
+
+// gets NBA data from localStorage
+function readNBAFromStorage() {
+  let stringData = localStorage.getItem('NBA');
+  let nba = JSON.parse(stringData) || [];
+  return nba;
+}
+// saves NBA data to localStorage
+function saveNBAToStorage(nba) {
+  let savedNBA = JSON.stringify(nba);
+  localStorage.setItem('NBA', savedNBA);
+}
 
 // gets login values from login form
 let loginInfo = JSON.parse(localStorage.getItem('logininfo'));
+console.log(loginInfo);
 // console.log(loginInfo);
 let loginUsername = $('#login-username').val();
 let loginPassword = $('#login-password').val();
@@ -51,13 +57,10 @@ const loginBtn = $('.login-btn');
 function checkLogin() {
   let loginUsername = $('#login-username').val();
   let loginPassword = $('#login-password').val();
-
   let match = loginInfo.filter(user => {
     return loginUsername === user.username && loginPassword === user.password;
   })
   if (match.length) {
-    getUserInfo();
-    getTeams();
     document.location.href = './homepage.html'
   } else {
     $(loginBtn).popover({
@@ -74,30 +77,35 @@ function getUserInfo() {
     password: $('#login-password').val(),
   }
   localStorage.setItem('userData', JSON.stringify(userData))
-  // checkLogin();
+  checkLogin();
 }
 
 // gets teams associated with login user to populate homepage
 function getTeams() {
-  let userData = JSON.parse(localStorage.getItem('userData'));
+
   for (i = 0; i < loginInfo.length; i++) {
     if (userData.username == loginInfo[i].username && userData.password == loginInfo[i].password) {
       userTeams.push(loginInfo[i].teams);
       localStorage.setItem('userTeams', JSON.stringify(userTeams))
+      localStorage.setItem('userTeams', JSON.stringify(userTeams))
     }
   }
-  // getUserInfo();
 }
+
+// calling getApi function
+getApi();
 
 // logout button
 $('#logout').on('click', function (e) {
   e.preventDefault();
+  console.log('logout')
+  window.location.replace('./login.html');
   // console.log('logout')
   window.location.replace('./index.html');
 });
 
 // login button
-loginBtn.on('click', function () {
-  // e.preventDefault();
-  checkLogin();
+loginBtn.on('click', function (e) {
+  e.preventDefault();
+  getUserInfo();
 })
